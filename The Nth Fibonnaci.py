27781197d06979635d1be388c4e0d779105cbb74
maamali-2004 <160328037+maamali-2004@users.noreply.github.com>
2025-04-23 13:46:20 +0330

    new files added

---
+++ The Nth Fibonnaci.py
@@ -0,0 +1,23 @@
+# def fibo(n):
+#      if n == 0 :
+#          return 0
+#      elif n == 1 :
+#          return 1
+#      else:
+#          return fibo(n-1) + fibo(n-2)
+# x = fibo(40)
+# print(x)
+# y = x % 10 
+# print(y)
+# def fibo(n):
+#     if n <= 0 :
+#         return "incorrect"
+#     if n > 2:
+#         data = [1, 1]
+#         for i in range(2, n):
+#             data.append(data[i-1] + data[i-2])
+#         return data[n-1]
+# x = fibo(40)
+# print(x)
+# y = x % 10 
+# print(y)
